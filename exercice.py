5d8c4fac5d754aa584b014a6958459b36073068f
anto252 <antoinecc@hotmail.ca>
2020-09-30 18:26:40 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -44,7 +44,7 @@ def histogram(sentence: str) -> tuple:
     # TODO: Créer l'histogramme a l'aide d'un dictionnaire
     #       Afficher l'histogramme et les lettres les plus fréquentes
     #       Retourner l'histogramme et le tableau de lettres
-    sentence = intput("saisir une phrase :")
+    sentence = input("saisir une phrase :")
 
     for letter in sentence:
 
